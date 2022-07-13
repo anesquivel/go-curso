550ce02f9d575a70b7ad82c689c853d903cbdcc8
anesquivel <andrea.esquivel@mercadolibre.com.mx>
2022-07-13 17:06:39 -0500

    Adding a practice for exam

--- cards/deck.go
+++ cards/deck.go
@@ -9,9 +9,25 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
 func (d deck) print() {
 
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,11 @@
 package main
 
 func main() {
-	//var card string = "Ace of Spades"
-	//card := newCard()
-	//card = "Five of Diamonds"
-	//fmt.Println(card)
 
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 	cards.print()
-}
 
-func newCard() string {
-	return "Five of Diamonds"
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 }
