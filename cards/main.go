e77988daf1d51cff760a645443672cbd67bbe2c2
anesquivel <andrea.esquivel@mercadolibre.com.mx>
2022-07-27 19:31:17 -0500

    trabajando con byte slices

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,12 @@ package main
 func main() {
 
 	cards := newDeck()
+
+	cards.saveToFile("my_cards")
+	/*fmt.Println(cards.toString())
 	cards.print()
 
 	hand, remainingCards := deal(cards, 5)
 	hand.print()
-	remainingCards.print()
+	remainingCards.print()*/
 }
